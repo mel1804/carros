7d27e5400ffb8681c04f27894737c5727fe15f52
Melissa Domingos <melissadomingos1804@gmail.com>
2024-03-28 00:27:50 -0300

    terceiro commit do App

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+   colors: {
+    'orange': '#e28625',
+    'green': '#006971',
+    'dark-green': '#004140',
+    'offwhite': 'rgba(255, 255, 255, 0.75)',
+    'white': '#fff',
+    'offwhitebg': '#f2f2f2'
+   },
+   fontFamily: {
+    'shoulders': '"Big Shoulders Display"',
+    'lexend': '"Lexend Deca"'
+   }
   },
+  
   plugins: [],
   }
 
